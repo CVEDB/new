c4f8be9b71f1f87d1fcdc716478d59feae842278
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-04-04 19:21:45 -0700

    Update generate_cve_list.py
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- generate_cve_list.py
+++ generate_cve_list.py
@@ -2,6 +2,7 @@ import os
 import requests
 import zipfile
 import datetime
+import json
 from github import Github
 
 # Define the API endpoint for the CVE Services API
@@ -16,7 +17,8 @@ def get_api_key(api_key_secret_name):
 def get_all_cves(api_key):
     response = requests.get(API_URL, headers={"API_KEY": api_key})
     if response.status_code == 200:
-        return response.json()['result']['CVE_Items']
+        all_cves = response.json()['result']['CVE_Items']
+        return all_cves
     else:
         raise ValueError(f"Failed to retrieve all CVEs. Response code: {response.status_code}")
         
@@ -24,7 +26,8 @@ def get_delta_cves(api_key, start_time):
     url = f'{API_URL}?modStartDate={start_time}'
     response = requests.get(url, headers={"API_KEY": api_key})
     if response.status_code == 200:
-        return response.json()['result']['CVE_Items']
+        delta_cves = response.json()['result']['CVE_Items']
+        return delta_cves
     else:
         raise ValueError(f"Failed to retrieve delta CVEs. Response code: {response.status_code}")
 
@@ -34,7 +37,8 @@ def create_zip_file(file_name, file_list):
             zip_file.write(file)
     print(f'Generated {file_name} with {len(file_list)} files')
 
-def create_cve_files(api_key):
+def create_cve_files(api_key, directory):
+    # Get all CVEs and delta CVEs
     all_cves = get_all_cves(api_key)
     delta_cves = get_delta_cves(api_key, datetime.datetime.utcnow().replace(microsecond=0).isoformat() + 'Z')
 
@@ -43,55 +47,22 @@ def create_cve_files(api_key):
     all_cves_file_name = f"{date_prefix}_all_CVEs.zip"
     delta_cves_file_name = f"{date_prefix}_delta_CVEs.zip"
 
+    # Create the directory structure
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    os.makedirs(f"{directory}/recent_activities")
+    for year in range(1999, datetime.datetime.today().year + 1):
+        os.makedirs(f"{directory}/{year}")
+        for cve_id in range(1, 10000):
+            os.makedirs(f"{directory}/{year}/{str(cve_id).zfill(4)}")
+
     # Create the all CVEs zip file
-    with open(f'{date_prefix}_all_CVEs.json', 'w') as f:
+    with open(f"{directory}/recent_activities.json", 'w') as f:
         f.write(json.dumps(all_cves))
-    create_zip_file(all_cves_file_name, [f'{date_prefix}_all_CVEs.json'])
-    os.remove(f'{date_prefix}_all_CVEs.json')
+    create_zip_file(all_cves_file_name, [f"{directory}/recent_activities.json"])
+    os.remove(f"{directory}/recent_activities.json")
     
     # Create the delta CVEs zip file
-    with open(f'{date_prefix}_delta_CVEs.json', 'w') as f:
-        f.write(json.dumps(delta_cves))
-    create_zip_file(delta_cves_file_name, [f'{date_prefix}_delta_CVEs.json'])
-    os.remove(f'{date_prefix}_delta_CVEs.json')
-    
-    # Create the release notes file
-    release_notes_file_name = f"{date_prefix}_Release_Notes.txt"
-    with open(release_notes_file_name, 'w') as f:
-        f.write("Release Notes for CVE List")
-    create_zip_file(release_notes_file_name, [release_notes_file_name])
-    os.remove(release_notes_file_name)
-
-    return (all_cves_file_name, delta_cves_file_name)
-
-def commit_cve_files_to_repo(github_token, repo_full_name, branch_name, file_names):
-    g = Github(github_token)
-    repo = g.get_repo(repo_full_name)
-    branch = repo.get_branch(branch_name)
-    commit_title = f'Update CVE release list {datetime.datetime.now().strftime("%Y-%m-%d")}'
-    commit_message = 'Add new CVE release list'
-
-    # Remove old files
-    contents = repo.get_contents('')
-    for content_file in contents:
-        if content_file.type == 'file' and content_file.name.endswith('.zip'):
-            if content_file.name not in file_names:
-                print(f'Removing {content_file.name}')
-                repo.delete_file(content_file.path, commit_message, content_file.sha, branch=branch_name)
-
-    # Upload new files
-    for file_name in file_names:
-        with open(file_name, 'rb') as f:
-            content = f.read()
-
-        if repo.get_contents(file_name) is None:
-            print(f'Adding {file_name}')
-            repo.create_file(file_name, commit_message, content, branch=branch_name)
-        else:
-            print(f'Updating {file_name}')
-            repo.update_file(file_name, commit_message, content, repo.get_contents(file_name).sha, branch=branch_name)
-
-    # Commit the changes
-    tree = repo.get_git_tree(branch.commit.sha)
-    commit = repo.create_git_commit(commit_title, commit_message, tree, [branch.commit])
-    branch.edit(commit
+    with open(f"{directory}/{date_prefix}_delta_CVEs.json", 'w') as f:
+        f.write(json
